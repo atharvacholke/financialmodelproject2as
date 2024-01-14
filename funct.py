d2b44d384702fba4b3d639c09a6f2b338039e4b2
AtharvaCholke <atharvacholke406@gmail.com>
2024-01-14 18:23:44 +0530

    added a randome revenue calculator

--- funct.py
+++ funct.py
@@ -1,24 +1,27 @@
-instagram=5
-facebook=8.77
-googleadsense=3
-tiktok=9.16
+
+instagram=414.28
+facebook=726.66
+googleadsense=248.57
+tiktok=758.97
 
 
 
 
-def admarketingconversions(conversionrate,cost,platform):
+def admarketingconversions(conversionrate,cost,platform,cpm):
     print(platform,'conversionrate is ',conversionrate)
     views=0
-    if platform==instagram:
+    if platform=='instagram':
         views=(cost/instagram)*1000-((cost/instagram)*1000)%1
-    elif platform==facebook:
+    elif platform=='facebook':
         views=(cost/facebook)*1000-((cost/facebook)*1000)%1
-    elif platform==googleadsense:
+    elif platform=='googleadsense':
         views=((cost/googleadsense)*1000)-((cost/googleadsense)*1000)%1
+    elif platform=='tiktok':
+        views=cost/tiktok*1000-((cost/googleadsense)*1000)%1
     else:
-        views=(cost/tiktok)*1000-((cost/tiktok)*1000)%1
+        views=(cost/cpm)*1000-((cost/cpm)*1000)%1
         
-    print(views)
+    print(views,'views')
 
     return (views*conversionrate/100)-(views*conversionrate/100)%1
 
--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import funct as fn
 import numpy as np
 
+budget=0
 
+customer=fn.admarketingconversions(1,200,'instagram',200)
+print(customer,'last step')
 
-customer=fn.admarketingconversions(1,2,'tiktok')
-print(customer)
+#a=fn.influencermarketingconversions(6,2000)
+#print(a)
 
-a=fn.influencermarketingconversions(6,2000)
-print(a)
+potentialclients=18000000
 
 
 
 class campaign:
-    budget=100000
     
-    def __init__(self,budget,div):
+    def __init__(self,budget,divno):
         self.budget=budget
-        self.div=div
+        self.div=divno
+        i=0
+        adplat=('instagram','facebook','tiktok','googleadsense','gen1','gen2','gen3','gen4')
+        divid=('div1','div2','div3','div4','div5','div6','div7','div8')
+        divisions={'div1':0,'div2':0,'div3':0,'div4':0,'div5':0,'div6':0,'div7':0,'div8':0}
+        while i<divno:
+            div=budget/divno
+            asc=fn.admarketingconversions(0.5,div,adplat[i],800)
+            divisions.update({divid[i]:asc})
+            print(adplat[i],divisions[divid[i]])
+            i+=1
+        print("testover")
+        self.mp=divisions
+        print(divisions)
         
-        
-        
+    
+    def __str__(self):
+        pass
 
+
+test=campaign(100000,8)
+
+print(test)     
+        
